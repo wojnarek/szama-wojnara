02d742ed2082592d8d23e25fc907183c0cf6eea8
wojnarek <wojnarek99@gmail.com>
2025-05-21 19:32:26 +0200

    Added navbar and map component. Set up leaflet library to open map based on user location or default location

---
+++ frontend/src/components/Map.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="map-container relative w-full h-[90vh]">
+    <div id="map" class="absolute inset-0 w-full h-full"></div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
+onMounted(() => {
+  
+
+//default location, my city
+  const defaultLat = 50.316753
+  const defaultLng = 17.383472
+  const defaultZoom = 15
+
+  //set map to defualt location
+  const map = L.map('map').setView([defaultLat, defaultLng], defaultZoom)
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+  }).addTo(map)
+
+  //get user location
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords
+        map.setView([latitude, longitude], defaultZoom)
+
+      },
+      (err) => {
+        //access = no, no location :()
+        console.warn('Nie udało się pobrać lokalizacji:', err.message)
+      }
+    )
+  } else {
+    // browser does not support geolocation
+    console.warn('Twoja przeglądarka nie obsługuje geolokalizacji.')
+  }
+})
+</script>
+
+<style>
+
+.map-container {
+  width: 100%;
+  height: 90vh;
+  min-height: 300px;
+}
+#map {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav class="navbar">
+    <div class="container">
+      <div class="flex-1"></div>
+<div class="search-wrapper relative w-full flex justify-center items-center">
+  <svg
+    class="absolute left-4 top-1/2 -translate-y-1/2 text-gray-400 w-7 h-7 pointer-events-none"
+    fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"
+  >
+    <circle cx="11" cy="11" r="8" />
+    <line x1="21" y1="21" x2="16.65" y2="16.65" />
+  </svg>
+  <input
+    type="text"
+    v-model="search"
+    class="search-input w-full sm:w-80 md:w-96 max-w-lg h-12 p-3 rounded-full border border-gray-300 outline-none transition-all duration-300 bg-white text-gray-900 text-lg shadow focus:ring-2 focus:ring-blue-400 focus:scale-105 focus:shadow-lg focus:border-blue-400 pl-12"
+    placeholder="Wyszukaj lokalizację lub miejsce..."
+  />
+</div>
+      <div class="flex-1"></div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const search = ref('')
+const focused = ref(false)
+</script>
